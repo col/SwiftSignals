e17ac2283ea527ec7228c723ef883ad29855044d
Colin Harris <col.w.harris@gmail.com>
2015-07-20 21:04:59 +0800

    Initial Commit

---
+++ SwiftSignals/SwiftSignals.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSignals.h
+//  SwiftSignals
+//
+//  Created by Colin Harris on 20/7/15.
+//  Copyright © 2015 Colin Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSignals.
+FOUNDATION_EXPORT double SwiftSignalsVersionNumber;
+
+//! Project version string for SwiftSignals.
+FOUNDATION_EXPORT const unsigned char SwiftSignalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSignals/PublicHeader.h>
+
+
